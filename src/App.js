afb2b74fa2dab78bc8bc778754cf031faba060c8
Angel Ivan <angelhuayas@gmail.com>
2021-09-27 14:19:04 -0500

    setup title react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
 import Main from './pages/Main';
 function App() {
+  useEffect(() => {
+        document.title = 'Servipy'
+    }, []);
   return (
     <>
     <Main></Main>
